aa2dbec3b0c62c98818e79ae77f5f9f0d274f487
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2020-12-19 08:32:46 +0800

    Update proj2.sh

--- sample_hpc_scripts/proj2.sh
+++ sample_hpc_scripts/proj2.sh
@@ -2,7 +2,7 @@
 #SBATCH --verbose
 #SBATCH -p aquila
 #SBATCH --time=100:00:00
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --mem=12GB
 #SBATCH --mail-type=ALL # select which email types will be sent
 #SBATCH --mail-user=yjl450@nyu.edu # put your email here if you want emails
@@ -11,8 +11,8 @@
 #SBATCH --output=run_%A_%a.out # %A is SLURM_ARRAY_JOB_ID, %a is SLURM_ARRAY_TASK_ID
 #SBATCH --error=run_%A_%a.err
 
-# #SBATCH --gres=gpu:1 # uncomment this line to request for a gpu if your program uses gpu
-#SBATCH --constraint=cpu # use this if you want to only use cpu
+#SBATCH --gres=gpu:1 # uncomment this line to request for a gpu if your program uses gpu
+# # SBATCH --constraint=cpu # use this if you want to only use cpu
 
 # the sleep command will help with hpc issues when you have many jobs loading same files
 sleep $(( (RANDOM%10) + 1 ))
